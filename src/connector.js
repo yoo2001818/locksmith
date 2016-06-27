f5dbfa183cd47117827bb7cdcd9424c34c37e06c
Duknam Yoo <yoo2001818@gmail.com>
2016-06-27 11:38:41 +0900

    Implement pushing actions

---
+++ src/connector.js
@@ -0,0 +1,11 @@
+export default class Connector {
+  getHostId() {
+    
+  }
+  getClientId() {
+
+  }
+  push(queue, target) {
+
+  }
+}
--- src/synchronizer.js
+++ src/synchronizer.js
@@ -1,5 +1,5 @@
 export default class Synchronizer {
-  constructor(machine) {
+  constructor(machine, connector) {
     /**
      * The deterministic state machine to use. Note that it MUST be
      * deterministic, or the synchronization won't work at all! It shouldn't
@@ -15,20 +15,17 @@ export default class Synchronizer {
      * - loadState(state) - Overwrites the machine state with provided
      *   serializable (or serialized) data. performing loadState and getState
      *   must produce same state!
-     * - run(action) - Runs the machine with provided action. This may trigger
-     *   another action (such as feedback). If so, the machine object should
-     *   keep the another action in the queue. Single feedback action is
-     *   shared between all devices, so it should only be used if the actions
-     *   are being created differently for each device.
-     * - getActions() - Returns and empties the action queue.
+     * - run(action) - Runs the machine with provided action.
      */
     this.machine = machine;
+    // The connector interface, used to communicate with other devices.
+    this.connector = connector;
     // The default configuration to use. This needs to be set to communicate,
     // however, clients may automatically receive the configuration at
     // connection time if the protocol supports it.
     // The configuration object is pure JSON object, so it can be easily
     // serialized.
-    this.configuration = {
+    this.config = {
       // If true, the tick only triggers when there is an action. This is useful
       // if tick shouldn't happen often.
       dynamic: true,
@@ -58,34 +55,229 @@ export default class Synchronizer {
     };
     // The tick ID. This increments by 1 every tick, and is used to
     this.tickId = 0;
+    // The tick time data of last few (freezeWait / fixedTick) ticks.
+    // This is used to calculate the round trip time.
+    this.tickTime = [];
     // The input queue. This stores the actions that needs to be run, separated
     // by tick ID.
     this.inputQueue = [];
     // The output queue. This stores the actions triggered by UI / etc...,
     // and sends it to the server.
     this.outputQueue = [];
+    // The client list. Used by the host.
+    this.clients = {};
+    this.clientList = [];
     // Is the system frozen?
     this.frozen = true;
+    // Is the system started?
+    this.started = false;
+    // Is this a host?
+    this.host = false;
+    // Timer objects
+    this.dynamicPushTimer = null;
+    this.dynamicTickTimer = null;
+    this.fixedTickTimer = null;
+  }
+  addClient(client) {
+    this.clients[client.id] = client;
+    this.clientList.push(client);
+  }
+  removeClient(clientId) {
+    let client = this.clients[clientId];
+    this.clientList.splice(this.clientList.indexOf(client), 1);
+    // Or we can call remove
+    this.clients[clientId] = undefined;
+  }
+  // Only server should call this; clients will automatically call this
+  // when connected.
+  start() {
+    this.started = true;
+    if (this.host) {
+      this.addClient({
+        id: this.connector.getClientId(),
+        rtt: 0,
+        ackId: 0,
+        lastTime: Date.now()
+      });
+    }
+    if (this.config.dynamic) {
+      // If there are any backlogs in output buffer, process them now
+      this.doPush();
+      // If there are any backlogs in input buffer, process them too
+      if (this.host && this.inputQueue.length > 0) {
+        this.handleTick();
+      }
+    } else {
+      // Start the tick timer..
+      if (this.fixedTickTimer === null) {
+        this.fixedTickTimer = setInterval(() => this.handleTick(),
+          this.config.fixedTick);
+      }
+    }
+  }
+  // Stop the synchronizer. This will freeze all connected nodes.
+  stop() {
+    this.started = false;
+    if (!this.config.dynamic && this.fixedTickTimer !== null) {
+      // Clear the tick timer.
+      clearInterval(this.fixedTickTimer);
+      this.fixedTickTimer = null;
+    }
   }
   // Queues and triggers the action created from UI / etc, ...
-  trigger(action) {
+  push(action) {
+    this.outputQueue.push(action);
+    if (this.config.dynamic && this.dynamicPushTimer === null && this.started) {
+      this.dynamicPushTimer = setTimeout(() => {
+        this.dynamicPushTimer = null;
+        this.doPush();
+      }, this.config.dynamicPushWait);
+    }
+  }
+  // Actually pushes the data to the server. This is only required for
+  // dynamic mode.
+  doPush() {
+    if (!this.started || !this.config.dynamic) return;
+    if (this.outputQueue.length === 0) return;
+    if (this.host) {
+      this.handlePush(this.outputQueue, this.connector.getClientId());
+      this.outputQueue = [];
+    } else {
+      this.connector.push(this.outputQueue, this.connector.getHostId());
+      this.outputQueue = [];
+    }
+  }
+  // Handle push - add action, trigger tick, etc...
+  handlePush(actions, clientId) {
+    if (this.host) {
+      // Push to host is only available for dynamic mode, since acknowledge
+      // will send the data to host in fixed mode.
+      if (!this.config.dynamic) return;
+      let client = this.clients[clientId];
+      if (client == null) {
+        // Client doesn't exist. But where do we need to send the error?
+        // TODO Add error processing mechanism
+        throw new Error('Client ID ' + clientId + ' does not exist');
+      }
+      // Copy the contents to input queue. concat is pretty slow.
+      for (let i = 0; i < actions.length; ++i) {
+        this.inputQueue.push(actions[i]);
+      }
+      // Start the dynamic tick timer,
+      if (this.dynamicTickTimer === null && this.started) {
+        this.dynamicTickTimer = setTimeout(() => {
+          this.dynamicTickTimer = null;
+          this.handleTick();
+        }, this.config.dynamicTickWait);
+      }
+    } else {
+      // This means the host has sent the message...
+      if (this.inputQueue.length === 0 ?
+        (this.tickId + 1 !== actions.id) :
+        (this.inputQueue[this.inputQueue.length - 1].id + 1 !== actions.id)
+      ) {
+        throw new Error('Wrong tick data received; desync occurred?');
+      }
+      this.inputQueue.push(actions);
+      this.connector.ack({
+        id: actions.id,
+        actions: this.outputQueue
+      }, this.connector.getHostId());
+      this.outputQueue = [];
+      // Cancel push timer if exists.
+      if (this.config.dynamic && this.dynamicPushTimer != null) {
+        clearTimeout(this.dynamicPushTimer);
+        this.dynamicPushTimer = null;
+      }
+      // Handle tick immedately in dynamic mode.
+      if (this.config.dynamic) this.handleTick();
+    }
   }
   // Handles the tick - run state machine, empty queue, etc...
   handleTick() {
+    if (this.host) {
+      let currentTime = Date.now();
+      // Host should check whether to hang, send input buffer to clients,
+      // then process itself.
+      for (let i = 0; i < this.clientList.length; ++i) {
+        let client = this.clientList[i];
+        if (client.lastTime + this.config.disconnectWait < currentTime) {
+          // Forcefully disconnect the client
+          // TODO
+        }
+        if (client.lastTime + this.config.freezeWait < currentTime) {
+          // Freeze...
+          this.doFreeze();
+          return;
+        }
+      }
+      // If it was frozen, unfreeze it now
+      if (this.frozen) this.doUnfreeze();
+      // Increment the tick ID
+      this.tickId ++;
+      this.tickTime.push(currentTime);
+      // Remove tickTime entry until specified length is reached
+      while (this.tickTime.length >
+        this.config.freezeWait / this.config.fixedTick
+      ) {
+        this.tickTime.shift();
+      }
+      // Now, push the input buffer to the clients.
+      let sendData = {
+        id: this.tickId,
+        actions: this.inputQueue
+      };
+      for (let i = 0; i < this.clientList.length; ++i) {
+        this.connector.push(sendData, this.clientList[i].id);
+      }
+      // Done! process the data, and empty the input queue.
+      for (let i = 0; i < this.inputQueue.length; ++i) {
+        let action = this.inputQueue[i];
+        this.machine.run(action);
+      }
+      this.inputQueue = [];
+      // We're all done!
+    } else {
+      // Do we have sufficient tick data? If not, freeze!
+      if ((!this.config.dynamic &&
+        this.inputQueue.length <= this.config.fixedBuffer) ||
+        this.inputQueue.length === 0
+      ) {
+        // Insufficient data, freeze.
+        this.doFreeze();
+        return;
+      }
+      if (this.frozen) {
+        this.doUnfreeze();
+      }
+      // Process input queue until we have no data remaining.
+      while (this.inputQueue.length > this.config.fixedBuffer) {
+        let frame = this.inputQueue.shift();
+        this.tickId = frame.id;
+        for (let i = 0; i < frame.length; ++i) {
+          this.machine.run(frame.actions[i]);
+        }
+      }
+      // Since machine will call push function by itself, we don't need to
+      // handle it.
+    }
+  }
+  doFreeze() {
+    this.frozen = true;
+  }
+  doUnfreeze() {
+    this.frozen = false;
   }
   // Handle connection - add client, freeze, etc...
-  handleConnect(state, issuer) {
+  handleConnect(state, clientId) {
   }
   // Handle connection ack - calculate RTT, trigger action, etc...
-  handleConnectAck(issuer) {
+  handleConnectAck(clientId) {
   }
   // Handle disconnect - remove client, trigger action, etc...
-  handleDisconnect(issuer) {
+  handleDisconnect(clientId) {
   }
   // Handle acknowledge - calculate RTT, add action, trigger tick, etc...
-  handleAck(actions, issuer) {
-  }
-  // Handle push - add action, trigger tick, etc...
-  handlePush(actions, issuer) {
+  handleAck(actions, clientId) {
   }
 }
